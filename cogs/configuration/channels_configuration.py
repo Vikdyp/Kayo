15d05eff2e9600c2476331f7b0b7e4ba5100d3c4
vikdyp <victor@roubin.net>
2026-01-19 21:43:19 +0100

    feat: add 'rank_up' notification channel option to configuration

--- cogs/configuration/channels_configuration.py
+++ cogs/configuration/channels_configuration.py
@@ -48,7 +48,8 @@ class ChannelsConfiguration(commands.Cog):
         ("voice_cleaner_category", "Categorie nettoyee (voice cleaner)"),
         ("voice_cleaner_afk", "Salon AFK a exclure (voice cleaner)"),
         ("matchmaking_voice_category", "Categorie vocale matchmaking"),
-        ("deban_category", "Categorie des demande de deban")
+        ("deban_category", "Categorie des demande de deban"),
+        ("rank_up", "Salon de notif de rang"),
     ]
 
     ACTION_CHOICES = [
